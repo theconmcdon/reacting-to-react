e0b725c4e62b77e89a33da6aae88bfed00ef8069
theconmcdon <83612766+theconmcdon@users.noreply.github.com>
2021-06-29 11:56:51 -0500

    HOW DO YOU COMMIT ANY OF THIS

---
+++ src/components/Greeter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Greeter = props => {
+
+
+    return (
+        <div>
+            <h1>{props.phrase}, {props.name}!</h1>
+        </div>
+    )
+};
+
+
+
+
+
+
+
+
+export default Greeter;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Loader = () => {
+
+
+    return (
+        <div>
+            <h1>Website loading...</h1>
+        </div>
+    )
+};
+
+
+
+
+
+
+
+
+export default Loader;
