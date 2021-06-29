1e364317ca9074f76f76e1eb1eeebe0a492cccf5
theconmcdon <83612766+theconmcdon@users.noreply.github.com>
2021-06-29 14:36:07 -0500

    added bootstrap styles

---
+++ src/components/Greeter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Greeter = props => {
+
+
+    return (
+        <div>
+            <h1 class='display-1 text-center'>{props.phrase}, {props.name}!</h1>
+        </div>
+    )
+};
+
+
+
+
+
+
+
+
+export default Greeter;
