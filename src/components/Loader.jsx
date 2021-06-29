3ed6ec5e3fd86449b49c794637f9b138e8e2f433
theconmcdon <83612766+theconmcdon@users.noreply.github.com>
2021-06-29 14:36:59 -0500

    added bootstrap styles

---
+++ src/components/Loader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const Loader = () => {
+
+
+    return (
+        <div>
+            <h1 class='display-1 text-center'>Website loading...</h1>
+        </div>
+    )
+};
+
+
+
+
+
+
+
+
+export default Loader;
